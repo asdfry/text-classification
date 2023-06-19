c1cb24e0a5064dd1ab595e7b1e89915c3d54df59
asdfry <asdfry@naver.com>
2023-06-19 14:48:39 +0000

    Update for multi-gpu

---
+++ train_multi.py
@@ -0,0 +1,143 @@
+import csv
+import torch
+import argparse
+import evaluate
+
+from torch.optim import AdamW
+from accelerate import Accelerator
+from torch.utils.data import DataLoader
+from datasets import load_dataset, DatasetDict
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, get_scheduler
+
+
+# Instantiate one in an accelerator object
+accelerator = Accelerator()
+
+
+# Argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--epoch", type=int, required=True)
+parser.add_argument("-b", "--batch_size", type=int, required=True)
+parser.add_argument("-t", "--test", action="store_true", default=False)
+args = parser.parse_args()
+
+
+# Prefix
+model_name = "classla/xlm-roberta-base-multilingual-text-genre-classifier"
+
+
+# Create dataset
+datasets = DatasetDict()
+datasets["train"] = load_dataset("csv", data_files="/mnt/data/train.csv")["train"]
+datasets["valid"] = load_dataset("csv", data_files="/mnt/data/valid.csv")["train"]
+
+
+# Load tokenizer and tokenize
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+
+def tokenize_function(examples):
+    return tokenizer(examples["text"], max_length=512, truncation=True, padding="max_length")
+
+
+tokenized_datasets = datasets.map(tokenize_function, batched=True)
+
+
+# Postprocess for train with native pytorch
+tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+tokenized_datasets.set_format("torch")
+
+
+# Create dataloader
+if args.test:
+    accelerate.print("This process is test mode")
+    small_train_dataset = tokenized_datasets["train"].shuffle(seed=77).select(range(160))
+    small_valid_dataset = tokenized_datasets["valid"].shuffle(seed=77).select(range(20))
+    train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=args.batch_size)
+    valid_dataloader = DataLoader(small_valid_dataset, batch_size=args.batch_size)
+else:
+    train_dataloader = DataLoader(tokenized_datasets["train"], shuffle=True, batch_size=args.batch_size)
+    valid_dataloader = DataLoader(tokenized_datasets["valid"], batch_size=args.batch_size)
+
+
+# Prepare id-label mapper
+with open("/mnt/data/id_to_label.csv", "r") as f:
+    reader = csv.DictReader(f)
+    id2label = {row["id"]: row["label"] for row in reader}
+label2id = {label: id for id, label in id2label.items()}
+
+
+# Load model
+model = AutoModelForSequenceClassification.from_pretrained(
+    model_name,
+    num_labels=18,
+    id2label=id2label,
+    label2id=label2id,
+    ignore_mismatched_sizes=True,
+)
+
+
+# Set optimizer
+optimizer = AdamW(model.parameters(), lr=5e-5)
+
+
+# Create learning rate scheduler
+num_training_steps = args.epoch * len(train_dataloader)
+lr_scheduler = get_scheduler(
+    name="linear",
+    optimizer=optimizer,
+    num_warmup_steps=0,
+    num_training_steps=num_training_steps,
+)
+
+
+# Load metric method
+metric = evaluate.load("accuracy")
+
+
+# Ready for training
+model, optimizer, train_dataloader, valid_dataloader, lr_scheduler = accelerator.prepare(
+    model, optimizer, train_dataloader, valid_dataloader, lr_scheduler
+)
+
+
+# Train
+for epoch in range(args.epoch):
+    accelerate.print("-" * 20, f"epoch {epoch + 1}", "-" * 20)
+
+    model.train()
+    loss_per_epoch = 0
+    for step, batch in enumerate(train_dataloader):
+        batch = {k: v.to(device) for k, v in batch.items()}
+        outputs = model(**batch)
+        loss = outputs.loss
+        loss_per_epoch += loss
+        accelerator.backward(loss)
+
+        optimizer.step()
+        lr_scheduler.step()
+        optimizer.zero_grad()
+        accelerate.print(f"[train] step: {step + 1}/{len(train_dataloader)}, loss: {loss_per_epoch / (step + 1)}")
+
+    model.eval()
+    loss_per_epoch = 0
+    for step, batch in enumerate(valid_dataloader):
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+
+        loss_per_epoch += outputs.loss
+        accelerate.print(f"[valid] step: {step + 1}/{len(valid_dataloader)}, loss: {loss_per_epoch / (step + 1)}")
+
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+        all_predictions, all_targets = accelerator.gather_for_metrics((predictions, batch))
+        metric.add_batch(all_predictions, all_targets)
+        metric.add_batch(predictions=predictions, references=batch["labels"])
+
+    accelerate.print(f"metric: {metric.compute()}")
+
+    save_path = f"/mnt/models/epoch-{epoch + 1}"
+    model.save_pretrained(save_path)
+    accelerate.print(f"model saved at {save_path}")
