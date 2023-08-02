c7aea0febcd9d96051017c842941669fe89c69a8
asdfry <asdfry@naver.com>
2023-08-02 14:00:43 +0900

    Update create_config for horovod

--- horovod/create_config.py
+++ horovod/create_config.py
@@ -1,25 +1,37 @@
 def write_master_config(network_addr: str, host_addr: int, port: int):
     hostname = f"{network_addr}.{host_addr}"
-    with open("config", "a") as f:
-        f.write(f"Host master\n")
-        f.write(f"    HostName {hostname}\n")
-        f.write(f"    Port {port}\n")
-        f.write(f"    User {user}\n")
-        f.write(f"    IdentityFile {identity_file}\n")
-        f.write(f"    StrictHostKeyChecking no\n\n")
+    # with open("config", "a") as f:
+    #     f.write(f"Host master\n")
+    #     f.write(f"    HostName {hostname}\n")
+    #     f.write(f"    Port {port}\n")
+    #     f.write(f"    User {user}\n")
+    #     f.write(f"    IdentityFile {identity_file}\n")
+    #     f.write(f"    StrictHostKeyChecking no\n\n")
+    with open("run.sh", "a") as f:
+        f.write(f"horovodrun -np $1 -H master:1,\\\n")
 
 
 def write_worker_config(network_addr: str, host_addr: int, port: int):
     global worker_num
     hostname = f"{network_addr}.{host_addr}"
-    with open("config", "a") as f:
-        f.write(f"Host worker-{worker_num}\n")
-        f.write(f"    HostName {hostname}\n")
-        f.write(f"    Port {port}\n")
-        f.write(f"    User {user}\n")
-        f.write(f"    IdentityFile {identity_file}\n")
-        f.write(f"    StrictHostKeyChecking no\n\n")
+    # with open("config", "a") as f:
+    #     f.write(f"Host worker-{worker_num}\n")
+    #     f.write(f"    HostName {hostname}\n")
+    #     f.write(f"    Port {port}\n")
+    #     f.write(f"    User {user}\n")
+    #     f.write(f"    IdentityFile {identity_file}\n")
+    #     f.write(f"    StrictHostKeyChecking no\n\n")
+    with open("run.sh", "a") as f:
+        f.write(f"worker-{worker_num}:1,")
     worker_num += 1
+
+def write_middle_line():
+    with open("run.sh", "a") as f:
+        f.write(f"\\\n")
+
+def write_last_line():
+    with open("run.sh", "a") as f:
+        f.write(f"python3 train_mulit.py -e 1 -b 4 -g 1 -hf\n")
 
 
 if __name__ == "__main__":
@@ -34,11 +46,14 @@ if __name__ == "__main__":
     write_master_config(network_addr, host_addr, 1041)
     for i in range(1, slot_count):
         write_worker_config(network_addr, host_addr + i, 1041 + i)
+    write_middle_line()
 
     while True:
         host_addr = int(input("Input Worker's Host Address: "))
         if host_addr == -1:
+            write_last_line()
             print("Exit")
             break
         for i in range(0, slot_count):
             write_worker_config(network_addr, host_addr + i, 1041 + i)
+        write_middle_line()
